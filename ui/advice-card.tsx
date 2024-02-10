1a939ae7b689dabda94551b3e522fd714901b74a
halvaradop_ <hernanvid123@gmail.com>
2024-02-10 16:41:55 -0500

    interactive creation of a tip

--- ui/advice-card.tsx
+++ ui/advice-card.tsx
@@ -14,14 +14,20 @@ import diceIcon from "@/public/icon-dice.svg"
 const AdviceCard = () => {
     const [advice, setAdvice] = useState<Advice>()
 
+
+    /**
+     * Creates a request to the API to obtain a hint to be consumed by the user
+     * 
+     * @returns {Promise<Advice>} successful promises of a advice
+     */
+    const getAdvice = async (): Promise<Advice> => {
+        const response = await fetch(process.env.NEXT_PUBLIC_URL_API_ADVICE!)
+        const json = await response.json() as Advice
+        setAdvice(json)
+        return json
+    }
+
     useEffect(() => {
-
-        const getAdvice = async () => {
-            const response = await fetch(process.env.NEXT_PUBLIC_URL_API_ADVICE!)
-            const json = await response.json() as Advice
-            setAdvice(json)
-        }
-
         getAdvice()
     }, [])
 
@@ -31,7 +37,7 @@ const AdviceCard = () => {
             <h1 className="text-green text-lg tracking-wider">ADVICE # {advice?.slip.id}</h1>
             <p className="mt-4 mb-10 text-cyan text-3xl">"{advice?.slip.advice}"</p>
             <Image className="mx-auto" src={dividerMobile} alt="divider image" priority />
-            <figure className="w-14 h-14 mx-auto grid place-content-center rounded-full absolute inset-x-0 -bottom-7 shadow-dice bg-green">
+            <figure className="w-14 h-14 mx-auto grid place-content-center rounded-full absolute inset-x-0 -bottom-7 shadow-dice bg-green hover:cursor-pointer" onClick={() => getAdvice()}>
                 <Image src={diceIcon} alt="dice icon" />
             </figure>
         </article>
